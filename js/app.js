7414d3758e54bf70de5c2490000ebc5a94565465
Guilherme-c-o <gcoliveira00@outlook.com>
2025-05-14 14:09:56 -0300

    script melhorado

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+let jogosAlugados = 0;
+
+function imprimirJogosAlugados(){
+    console.log(`Total de jogos alugados: ${jogosAlugados}`);
+}
 
 
 function alterarStatus(id){
@@ -7,12 +12,26 @@ function alterarStatus(id){
 
 
     if (imgDiv.classList.contains('dashboard__item__img--rented')){
+
+        let confirma = window.confirm('Você tem certeza que deseja devolver o jogo?');
+        if (!confirma){
+            return;
+        }
+
         imgDiv.classList.remove('dashboard__item__img--rented');
         button.classList.remove('dashboard__item__button--return');
         button.innerHTML = 'Alugar';
+        jogosAlugados--;
     } else {
         imgDiv.classList.add('dashboard__item__img--rented');
         button.classList.add('dashboard__item__button--return');
         button.innerHTML = 'Devolver';
+        jogosAlugados++;
     }
+    imprimirJogosAlugados();
 }
+
+document.addEventListener('DOMComentLoaded', function(){
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    imprimirJogosAlugados();
+})
